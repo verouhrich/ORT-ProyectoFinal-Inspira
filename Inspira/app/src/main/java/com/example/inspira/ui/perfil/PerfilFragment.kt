0913ab3fcc15fe75d1aa43f418a965a61cb2375d
FEDERICO GUILLERMO LAVISTA SEGUI <30494796@comunidadort.edu.ar>
2020-09-26 20:35:34 -0300

    IN-37 - Tips arreglado y wip de Perfil

---
+++ Inspira/app/src/main/java/com/example/inspira/ui/perfil/PerfilFragment.kt
@@ -0,0 +1,32 @@
+package com.example.inspira.ui.perfil
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.example.inspira.R
+
+
+class TipsFragment : Fragment() {
+
+    private lateinit var perfilViewModel: PerfilViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        perfilViewModel =
+            ViewModelProviders.of(this).get(PerfilViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_tips, container, false)
+        val textView: TextView = root.findViewById(R.id.text_tips)
+        perfilViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
--- Inspira/app/src/main/java/com/example/inspira/ui/perfil/PerfilViewModel.kt
+++ Inspira/app/src/main/java/com/example/inspira/ui/perfil/PerfilViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.inspira.ui.Tips
+package com.example.inspira.ui.perfil
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class TipsViewModel : ViewModel() {
+class PerfilViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is tips Fragment"
+        value = "This is the Perfil Fragment"
     }
     val text: LiveData<String> = _text
 }
