c50a497aa6d1b6def2fb83097af7cfc9d77e0857
German Sandoval <gsandoval@HEXACTA.COM>
2020-11-07 03:32:22 -0300

    zero notifications message

--- Inspira/app/src/main/java/com/ort/inspira/FirebaseServices.kt
+++ Inspira/app/src/main/java/com/ort/inspira/FirebaseServices.kt
@@ -4,12 +4,14 @@ import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.iid.FirebaseInstanceId
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.messaging.FirebaseMessaging
 import com.ort.inspira.ui.notifications.HistoryDocument
 import com.ort.inspira.ui.notifications.Notification
+import org.w3c.dom.Document
 import java.io.IOException
 
 class FirebaseServices {
@@ -38,7 +40,7 @@ class FirebaseServices {
         firebaseAuth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
-                    val firebaseUser: FirebaseUser? = task!!.result!!.user
+                    val firebaseUser: FirebaseUser? = task.result?.user
                     returnData(firebaseUser)
                 } else {
                     returnData(null)
@@ -47,10 +49,10 @@ class FirebaseServices {
     }
 
     fun getTopic(user: FirebaseUser, returnData: (String?) -> Unit) {
-        usersRef.document(user.uid).get().addOnCompleteListener { task ->
+        usersRef.document(user!!.uid).get().addOnCompleteListener { task ->
             if (task.isSuccessful) {
-                if (task.result!!.exists() && task.result!!.get("topic") != null) {
-                    val topic = task.result!!.get("topic") as String
+                if (task.result!!.exists() && task.result?.get("topic") != null) {
+                    val topic: String? = task.result?.get("topic") as String?
                     returnData(topic)
                 } else {
                     returnData(null)
@@ -75,16 +77,20 @@ class FirebaseServices {
         }
     }
 
-    fun getNotificationHistoryByTopic(topic: String, returnData: (List<Notification>) -> Unit) {
+    fun getNotificationHistoryByTopic(topic: String?, returnData: (List<Notification>?) -> Unit) {
+        if (topic.isNullOrEmpty()) returnData(emptyList())
         historyRef.whereEqualTo("topic", topic)
             .limit(1)
             .get().addOnCompleteListener { task ->
-                if (task.isSuccessful && task.result!!.documents.isNotEmpty()) {
-                    val notifications: List<Notification> = task.result!!
-                        .documents[0].toObject(HistoryDocument::class.java)!!.notifications
-                    returnData(notifications)
+                if (task.isSuccessful) {
+                    val documents: List<DocumentSnapshot> = task.result!!.documents
+                    if (documents.isNotEmpty()) {
+                        returnData(documents[0].toObject(HistoryDocument::class.java)?.notifications)
+                    } else {
+                        returnData(emptyList())
+                    }
                 } else {
-                    returnData(emptyList())
+                    returnData(null)
                 }
             }
     }
--- Inspira/app/src/main/java/com/ort/inspira/ui/notifications/NotificationsFragment.kt
+++ Inspira/app/src/main/java/com/ort/inspira/ui/notifications/NotificationsFragment.kt
@@ -4,17 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.ktx.Firebase
 import com.ort.inspira.FirebaseServices
 import com.ort.inspira.R
+import com.ort.inspira.ui.home.HomeViewModel
 
 class NotificationsFragment : Fragment() {
 
     private lateinit var firebaseServices: FirebaseServices
+    private lateinit var notificationsViewModel: NotificationsViewModel
     private var topic: String? = null
+    private lateinit var emptyListMessage: TextView
+    private lateinit var root: View
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -23,16 +31,32 @@ class NotificationsFragment : Fragment() {
     ): View? {
         topic = activity?.intent?.getStringExtra("topic")
         firebaseServices = FirebaseServices()
-        return inflater.inflate(R.layout.fragment_notifications, container, false)
+        root = inflater.inflate(R.layout.fragment_notifications, container, false)
+        emptyListMessage = root.findViewById(R.id.emptyList)
+        notificationsViewModel = ViewModelProviders.of(this).get(NotificationsViewModel::class.java)
+        return root
     }
 
     override fun onViewCreated(itemView: View, savedInstanceState: Bundle?) {
         super.onViewCreated(itemView, savedInstanceState)
         val recyclerView: RecyclerView? = view?.findViewById(R.id.recyclerView)
         recyclerView?.layoutManager = LinearLayoutManager(context)
-        firebaseServices.getNotificationHistoryByTopic(topic!!) { notifications ->
-            recyclerView?.adapter = NotificationAdapter(notifications)
+        firebaseServices.getNotificationHistoryByTopic(topic) { notifications ->
+            if (notifications != null) {
+                if (notifications.isNotEmpty()) {
+                    recyclerView?.adapter = NotificationAdapter(notifications)
+                } else {
+                    notificationsViewModel.emptyList.observe(viewLifecycleOwner, Observer {
+                        emptyListMessage.text = it
+                    })
+                }
+            } else {
+                showLongToast(getString(R.string.notificationsError))
+            }
         }
     }
 
+    private fun showLongToast(message: String) {
+        Toast.makeText(activity?.baseContext, message, Toast.LENGTH_LONG).show()
+    }
 }
