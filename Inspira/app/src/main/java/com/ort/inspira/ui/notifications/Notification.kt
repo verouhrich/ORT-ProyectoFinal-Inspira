8cbe20564b90dac09db3d16dce7c04dc9eddc6c4
German Sandoval <gsandoval@hexacta.com>
2020-11-24 22:06:25 -0300

    background colors

--- Inspira/app/src/main/java/com/ort/inspira/ui/notifications/Notification.kt
+++ Inspira/app/src/main/java/com/ort/inspira/ui/notifications/Notification.kt
@@ -4,6 +4,6 @@ import com.google.firebase.Timestamp
 import java.text.SimpleDateFormat
 import java.util.*
 
-data class Notification(var title: String, var body: String, var date: Timestamp) {
-    constructor() : this("Default Title","Default Body", Timestamp(0, 0 ))
+data class Notification(var title: String, var body: String, var color: String, var date: Timestamp) {
+    constructor() : this("Default Title","Default Body", "#616161", Timestamp(0, 0 ))
 }
